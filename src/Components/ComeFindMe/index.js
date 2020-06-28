5b34d68ce003416c5a6cc6314d5a101f131a3076
Nicolas Conaway <n.conaway00@gmail.com>
2020-06-28 00:48:05 -0400

    fifth commit

--- src/Components/ComeFindMe/index.js
+++ src/Components/ComeFindMe/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './styles.css';
+import Linkedin from './icons/linkedin2.png'
+import Github from './icons/github2.png'
 
 class Social extends React.Component {
 
@@ -7,7 +9,25 @@ class Social extends React.Component {
         return (
             <>
                 <div className="social">
-                    <h1>Social Media Here!!!</h1>
+                    <h1>My Social Media!</h1>
+
+                    <div className="social__container">
+                        <a href="https://www.linkedin.com/in/nicolas-conaway/" 
+                        className="social__card" 
+                        target="_blank">
+                            <img src={Linkedin} />
+                            <p>LinkedIn</p>
+                        </a>
+
+                        <a href="https://github.com/NicolasConaway" 
+                        className="social__card" 
+                        target="_blank">
+                            <img src={Github} />
+                            <p>Github</p>
+                        </a>
+                    </div>
+
+
                 </div>
             </>
         )
