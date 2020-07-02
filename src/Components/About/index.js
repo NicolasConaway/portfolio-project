edd29eb9ab2c79038486d389c8f5e02d3d67dd81
Nicolas Conaway <n.conaway00@gmail.com>
2020-07-02 10:27:54 -0400

    nineth

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles.css';
 import Me from './nicolas_conaway.jpg'
+import Download from './Files/ResumeNC.pdf'
 
 class About extends React.Component {
 
@@ -12,6 +13,10 @@ class About extends React.Component {
                     <h1>About Me</h1>
                     <img src={Me} alt="Nicolas Conaway" />
                     <p>Hello, my name is Nicolas Conaway. I am a Road to Hire student who's striving to become a front developer. Currently I am hooked onto the library, React!</p>
+                    <a href= {Download}
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    download>Download</a>
                 </div>
 
             </>
